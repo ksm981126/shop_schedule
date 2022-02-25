4c4a3f858b3efeb34aaca2065fde725698a396da
ksm981126 <ksm981126@naver.com>
2022-02-25 16:34:30 +0900

    파일 업로드

---
+++ src/main/java/com/twogenesis/shop_data_schedule/ShopDataScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.twogenesis.shop_data_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDataScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDataScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twogenesis/shop_data_schedule/ShopDataScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twogenesis.shop_data_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDataScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
